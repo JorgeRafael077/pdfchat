c2f7131737bd9da431b288adc2bcbeabeadf7ed5
JorgeRafael077 <jorgerafael1000h@gmail.com>
2025-05-24 19:27:31 -0300

    feat: complete github authentication implementation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,11 @@
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="font-work-sans">
         <Navbar />
-
+        
         {children}
     </main>
   );
-} 
+}
