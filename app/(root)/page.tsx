13d8175ee12e25d60d042592af976e7f45f08200
JorgeRafael077 <jorgerafael1000h@gmail.com>
2025-06-01 16:29:55 -0300

    Feat: Components, Hero Section, search form, and its functionalities

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
+import SearchForm from "../components/SeachForm";
 
-export default function Home() {
+export default async function Home({ searchParams} : { 
+  searchParams: Promise<{ query?: string }>;
+}) {
 
-  // falta mesclar a branch com a main
+  const query = (await searchParams).query 
 
   return (
       <>  
-        <h1 className="text-2xl">Home</h1>
+        <section className="pink_container">
+          <h1 className="heading">Mostre sua ideia, conecte-se, troque experiências e escreva sua própria história.</h1>
+
+          <p className="sub-heading !max-w-3xl">
+            Envie suas ideias, vote nas melhores propostas e ganhe destaque em competições online.
+          </p>
+
+          <SearchForm query={query}/>
+        </section>
+        
       </>
   );
 }
